4694154b85a2b2d801ebcce0ec5fb4192872ffb2
Darko Križić <darko@krizic.net>
2024-02-01 16:31:59 +0100

    All features enabled in Azure

--- terraform/azure/6-cloud-flare-dns.tf
+++ terraform/azure/6-cloud-flare-dns.tf
@@ -1,7 +1,7 @@
-#resource "cloudflare_record" "prodyna-wtf" {
-#  zone_id = var.cloudflare_zone_id
-#  name    = var.project_name
-#  value   = azurerm_public_ip.ingress.ip_address
-#  type    = "A"
-#  ttl     = 3600
-#}
+resource "cloudflare_record" "prodyna-wtf" {
+  zone_id = var.cloudflare_zone_id
+  name    = var.project_name
+  value   = azurerm_public_ip.ingress.ip_address
+  type    = "A"
+  ttl     = 3600
+}
