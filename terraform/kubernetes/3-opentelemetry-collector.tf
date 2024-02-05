6a27c3de07f2e5336b0097d1bc3b727b5f604edf
Darko Križić <darko@krizic.net>
2024-02-05 13:09:31 +0100

    Enable opentelemetry

--- terraform/kubernetes/3-opentelemetry-collector.tf
+++ terraform/kubernetes/3-opentelemetry-collector.tf
@@ -2,33 +2,33 @@
 # Enable log collection
 # Send traces, metrics and logs to Azure Monitor
 
-#resource "kubernetes_namespace" "observability" {
-#  metadata {
-#    name = "observability"
-#  }
-#}
-#
-#resource "kubernetes_secret" "azuremonitorr" {
-#  metadata {
-#    name      = "azuremonitor"
-#    namespace = kubernetes_namespace.observability.metadata[0].name
-#  }
-#  data = {
-#    "AZURE_MONITOR_INSTRUMENTATION_KEY" = data.terraform_remote_state.azure.outputs.appi_instrumentation_key
-#  }
-#  type = "Opaque"
-#}
-#
-#
-## https://artifacthub.io/packages/helm/opentelemetry-helm/opentelemetry-collector
-#resource "helm_release" "opentelemetry-collector" {
-#  repository       = "https://open-telemetry.github.io/opentelemetry-helm-charts"
-#  chart            = "opentelemetry-collector"
-#  name             = "opentelemetry-collector"
-#  namespace        = kubernetes_namespace.observability.metadata[0].name
-#  version          = "0.78.1"
-#  create_namespace = false
-#  force_update     = true
-#
-#  values = [file("assets/opentelemetry-collector/helm-values.yaml")]
-#}
+resource "kubernetes_namespace" "observability" {
+  metadata {
+    name = "observability"
+  }
+}
+
+resource "kubernetes_secret" "azuremonitorr" {
+  metadata {
+    name      = "azuremonitor"
+    namespace = kubernetes_namespace.observability.metadata[0].name
+  }
+  data = {
+    "AZURE_MONITOR_INSTRUMENTATION_KEY" = data.terraform_remote_state.azure.outputs.appi_instrumentation_key
+  }
+  type = "Opaque"
+}
+
+
+# https://artifacthub.io/packages/helm/opentelemetry-helm/opentelemetry-collector
+resource "helm_release" "opentelemetry-collector" {
+  repository       = "https://open-telemetry.github.io/opentelemetry-helm-charts"
+  chart            = "opentelemetry-collector"
+  name             = "opentelemetry-collector"
+  namespace        = kubernetes_namespace.observability.metadata[0].name
+  version          = "0.78.1"
+  create_namespace = false
+  force_update     = true
+
+  values = [file("assets/opentelemetry-collector/helm-values.yaml")]
+}
