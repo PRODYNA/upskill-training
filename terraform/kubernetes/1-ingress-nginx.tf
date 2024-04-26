19caf3d69b2cc007f80dfd36717dfbf287f6b99f
Darko Križić <darko@krizic.net>
2024-04-26 11:17:27 +0200

    Install ingress-controller

--- terraform/kubernetes/1-ingress-nginx.tf
+++ terraform/kubernetes/1-ingress-nginx.tf
@@ -1,35 +1,35 @@
-# resource "kubernetes_namespace" "ingress-nginx" {
-#   metadata {
-#     name = "ingress-nginx"
-#   }
-# }
-#
-# # https://kubernetes.github.io/ingress-nginx/
-# resource "helm_release" "ingress_nginx" {
-#   repository       = "https://kubernetes.github.io/ingress-nginx"
-#   chart            = "ingress-nginx"
-#   name             = "ingress-nginx"
-#   namespace        = kubernetes_namespace.ingress-nginx.metadata[0].name
-#   version          = "v4.10.0"
-#   force_update     = true
-#   create_namespace = false
-#
-#   # helm -n ingress-nginx upgrade --install ingress-nginx ingress-nginx/ingress-nginx --version 4.9.1 -f assets/ingress-nginx/helm-values.yaml --set a=1 --set a=2
-#
-#   set {
-#     name  = "controller.service.annotations.\"service\\.beta\\.kubernetes\\.io/azure-load-balancer-health-probe-request-path\""
-#     value = "/healthz"
-#   }
-#   set {
-#     name  = "controller.service.loadBalancerIP"
-#     value = data.azurerm_public_ip.ingress.ip_address
-#   }
-#   set {
-#     name  = "controller.service.annotations.\"service\\.beta\\.kubernetes\\.io/azure-load-balancer-resource-group\""
-#     value = data.azurerm_resource_group.main.name
-#   }
-#
-#   values = [
-#     file("assets/ingress-nginx/helm-values.yaml")
-#   ]
-# }
+resource "kubernetes_namespace" "ingress-nginx" {
+  metadata {
+    name = "ingress-nginx"
+  }
+}
+
+# https://kubernetes.github.io/ingress-nginx/
+resource "helm_release" "ingress_nginx" {
+  repository       = "https://kubernetes.github.io/ingress-nginx"
+  chart            = "ingress-nginx"
+  name             = "ingress-nginx"
+  namespace        = kubernetes_namespace.ingress-nginx.metadata[0].name
+  version          = "v4.10.0"
+  force_update     = true
+  create_namespace = false
+
+  # helm -n ingress-nginx upgrade --install ingress-nginx ingress-nginx/ingress-nginx --version 4.9.1 -f assets/ingress-nginx/helm-values.yaml --set a=1 --set a=2
+
+  set {
+    name  = "controller.service.annotations.\"service\\.beta\\.kubernetes\\.io/azure-load-balancer-health-probe-request-path\""
+    value = "/healthz"
+  }
+  set {
+    name  = "controller.service.loadBalancerIP"
+    value = data.azurerm_public_ip.ingress.ip_address
+  }
+  set {
+    name  = "controller.service.annotations.\"service\\.beta\\.kubernetes\\.io/azure-load-balancer-resource-group\""
+    value = data.azurerm_resource_group.main.name
+  }
+
+  values = [
+    file("assets/ingress-nginx/helm-values.yaml")
+  ]
+}
