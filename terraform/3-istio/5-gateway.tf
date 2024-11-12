951bac535aaaf66ac3daf80f7af07efa3336a338
Darko Križić <darko@krizic.net>
2024-11-12 14:58:27 +0100

    Limit traefik to standard resources

--- terraform/3-istio/5-gateway.tf
+++ terraform/3-istio/5-gateway.tf
@@ -1,87 +1,85 @@
-# resource "kubernetes_manifest" "gateway_bookinfo_gateway_external" {
-#   manifest = {
-#     "apiVersion" = "networking.istio.io/v1beta1"
-#     "kind" = "Gateway"
-#     "metadata" = {
-#       "name" = "bookinfo-gateway-external"
-#       "namespace" = kubernetes_namespace_v1.bookinfo.metadata[0].name
-#     }
-#     "spec" = {
-#       "selector" = {
-#         "istio" = "aks-istio-ingressgateway-external"
-#       }
-#       "servers" = [
-#         {
-#           "hosts" = [
-#             "*",
-#           ]
-#           "port" = {
-#             "name" = "http"
-#             "number" = 80
-#             "protocol" = "HTTP"
-#           }
-#         },
-#       ]
-#     }
-#   }
-# }
-#
-# resource "kubernetes_manifest" "virtualservice_bookinfo_vs_external" {
-#   manifest = {
-#     "apiVersion" = "networking.istio.io/v1beta1"
-#     "kind" = "VirtualService"
-#     "metadata" = {
-#       "name" = "bookinfo-vs-external"
-#       "namespace" = kubernetes_namespace_v1.bookinfo.metadata[0].name
-#     }
-#     "spec" = {
-#       "gateways" = [
-#         "bookinfo-gateway-external",
-#       ]
-#       "hosts" = [
-#         "*",
-#       ]
-#       "http" = [
-#         {
-#           "match" = [
-#             {
-#               "uri" = {
-#                 "exact" = "/productpage"
-#               }
-#             },
-#             {
-#               "uri" = {
-#                 "prefix" = "/static"
-#               }
-#             },
-#             {
-#               "uri" = {
-#                 "exact" = "/login"
-#               }
-#             },
-#             {
-#               "uri" = {
-#                 "exact" = "/logout"
-#               }
-#             },
-#             {
-#               "uri" = {
-#                 "prefix" = "/api/v1/products"
-#               }
-#             },
-#           ]
-#           "route" = [
-#             {
-#               "destination" = {
-#                 "host" = "productpage"
-#                 "port" = {
-#                   "number" = 9080
-#                 }
-#               }
-#             },
-#           ]
-#         },
-#       ]
-#     }
-#   }
-# }
+resource "kubernetes_manifest" "gateway_bookinfo_gateway" {
+  manifest = {
+    "apiVersion" = "gateway.networking.k8s.io/v1"
+    "kind" = "Gateway"
+    "metadata" = {
+      "name" = "bookinfo-gateway"
+      namespace = kubernetes_namespace_v1.bookinfo.metadata.0.name
+    }
+    "spec" = {
+      "gatewayClassName" = "istio"
+      "listeners" = [
+        {
+          "allowedRoutes" = {
+            "namespaces" = {
+              "from" = "Same"
+            }
+          }
+          "name" = "http"
+          "port" = 80
+          "protocol" = "HTTP"
+        },
+      ]
+    }
+  }
+}
+
+resource "kubernetes_manifest" "httproute_bookinfo" {
+  manifest = {
+    "apiVersion" = "gateway.networking.k8s.io/v1"
+    "kind" = "HTTPRoute"
+    "metadata" = {
+      "name" = "bookinfo"
+      namespace = kubernetes_namespace_v1.bookinfo.metadata.0.name
+    }
+    "spec" = {
+      "parentRefs" = [
+        {
+          "name" = "bookinfo-gateway"
+        },
+      ]
+      "rules" = [
+        {
+          "backendRefs" = [
+            {
+              "name" = "productpage"
+              "port" = 9080
+            },
+          ]
+          "matches" = [
+            {
+              "path" = {
+                "type" = "Exact"
+                "value" = "/productpage"
+              }
+            },
+            {
+              "path" = {
+                "type" = "PathPrefix"
+                "value" = "/static"
+              }
+            },
+            {
+              "path" = {
+                "type" = "Exact"
+                "value" = "/login"
+              }
+            },
+            {
+              "path" = {
+                "type" = "Exact"
+                "value" = "/logout"
+              }
+            },
+            {
+              "path" = {
+                "type" = "PathPrefix"
+                "value" = "/api/v1/products"
+              }
+            },
+          ]
+        },
+      ]
+    }
+  }
+}
